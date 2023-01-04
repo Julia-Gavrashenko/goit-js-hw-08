562ddf60acfbf07b2b4179192cb16229dc00543d
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-01-04 22:31:57 +0200

    sixth commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,34 @@
 import throttle from 'lodash.throttle';
 
-console.log("hello");
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
 
+form.addEventListener(`input`, throttle(onFormInput, 500));
+form.addEventListener(`submit`, onFormSubmit);
 
+const formData = {};
 
+getSavedMessage();
 
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(`feedback-form-state`, JSON.stringify(formData));
+  // console.log(formData);
+}
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem(`feedback-form-state`);
+}
 
+function getSavedMessage() {
+  const savedMessage = JSON.parse(localStorage.getItem(`feedback-form-state`));
+  // console.log(savedMessage);
+
+  if (savedMessage) {
+    input.value = savedMessage.email;
+    textarea.value = savedMessage.message;
+  }
+}
